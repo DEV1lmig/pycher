227a7ebf7e810585729c1b95f0cbce6a69c917ec
Miguel <maquiroz@urbe.edu.ve>
2025-05-11 14:50:14 -0400

    fixed course card

--- frontend/src/pages/home/Dashboard.jsx
+++ frontend/src/pages/home/Dashboard.jsx
@@ -1,9 +1,16 @@
 import DashboardLayout from "@/components/dashboard/DashboardLayout"
+import { useEffect, useState } from "react"
 import { CourseCards } from "@/components/dashboard/CourseCards"
 import { WelcomeHeader } from "@/components/dashboard/WelcomeHeader"
 import { StatsCards } from "@/components/dashboard/StatsCards"
+import { getAllCourses } from "@/services/contentService"
 
 export default function DashboardPage() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    getAllCourses().then(setCourses);
+  }, []);
   return (
     <DashboardLayout>
       <div className="flex flex-col gap-6 p-6">
@@ -14,7 +21,7 @@ export default function DashboardPage() {
           <p className="text-gray-300 mb-6">
             Selecciona el curso que deseas estudiar y comienza tu viaje en el mundo de Python
           </p>
-          <CourseCards />
+          <CourseCards courses={courses} />
         </section>
       </div>
     </DashboardLayout>
