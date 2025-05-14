73afd2f5ea2c364e5311f60a26617e59697f8108
Miguel <maquiroz@urbe.edu.ve>
2025-05-14 10:23:39 -0400

    removed garbage

--- backend/shared/alembic/env.py
+++ backend/shared/alembic/env.py
@@ -38,7 +38,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option("sqlalchemy.url". os.environ.get("DATABASE_URL"))
     context.configure(
         url=url,
         target_metadata=target_metadata,
